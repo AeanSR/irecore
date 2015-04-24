65fc1b7da42cb15988f813f169f5a400997df84c
Aean <v@aean.net>
2015-04-24 09:30:39 +0800

    Include LICENSE into release archive.

--- pack_release.bat
+++ pack_release.bat
@@ -6,3 +6,4 @@ copy %windir%\SYSWOW64\msvcp120.dll release\msvcp120.dll
 copy ic_tg_t17m.txt release\profile\ic_tg_t17m.txt
 copy irecore.exe release\irecore.exe
 copy kernel.c release\kernel.c
+copy LICENSE release\LICENSE

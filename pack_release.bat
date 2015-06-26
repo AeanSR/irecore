ef8abd388ffc3a1ee428decd4f8f8ad77b56d14c
Aean <v@aean.net>
2015-06-27 06:39:17 +0800

    Added dependency.

--- pack_release.bat
+++ pack_release.bat
@@ -4,6 +4,7 @@ set qt_dir=E:\Qt\5.4\msvc2013_opengl
 rd /S /Q release
 mkdir release
 mkdir release\profile
+mkdir release\platforms
 copy %windir%\SYSWOW64\msvcr120.dll release\msvcr120.dll
 copy %windir%\SYSWOW64\msvcp120.dll release\msvcp120.dll
 copy ic_tg_t17m.txt release\profile\ic_tg_t17m.txt
@@ -15,6 +16,7 @@ copy %qt_dir%\bin\Qt5Core.dll release\Qt5Core.dll
 copy %qt_dir%\bin\Qt5GUI.dll release\Qt5GUI.dll
 copy %qt_dir%\bin\Qt5Widgets.dll release\Qt5Widgets.dll
 copy %qt_dir%\bin\Qt5Network.dll release\Qt5Network.dll
+copy %qt_dir%\plugins\platforms\qwindows.dll release\platforms\qwindows.dll
 copy %qt_dir%\bin\icuin53.dll release\icuin53.dll
 copy %qt_dir%\bin\icuuc53.dll release\icuuc53.dll
 copy icudt53_min.dll release\icudt53.dll

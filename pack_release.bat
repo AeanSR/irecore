22d4732e82c2c735feba7854ecd3dfbcaa580c68
Aean <v@aean.net>
2015-06-30 11:51:03 +0800

    Static library linking. Hope this could fix the exception 0x40000015.

--- pack_release.bat
+++ pack_release.bat
@@ -5,8 +5,8 @@ rd /S /Q release
 mkdir release
 mkdir release\profile
 mkdir release\platforms
-copy %windir%\SYSWOW64\msvcr120.dll release\msvcr120.dll
-copy %windir%\SYSWOW64\msvcp120.dll release\msvcp120.dll
+:: copy %windir%\SYSWOW64\msvcr120.dll release\msvcr120.dll
+:: copy %windir%\SYSWOW64\msvcp120.dll release\msvcp120.dll
 copy ic_tg_t17m.txt release\profile\ic_tg_t17m.txt
 copy irecore.exe release\irecore.exe
 copy kernel.c release\kernel.c

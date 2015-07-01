2e19e77973177f8d0d009a1fa250b65aaf2aea21
Aean <v@aean.net>
2015-07-01 13:45:04 +0800

    T18 profile.

--- pack_release.bat
+++ pack_release.bat
@@ -8,6 +8,7 @@ mkdir release\platforms
 :: copy %windir%\SYSWOW64\msvcr120.dll release\msvcr120.dll
 :: copy %windir%\SYSWOW64\msvcp120.dll release\msvcp120.dll
 copy ic_tg_t17m.txt release\profile\ic_tg_t17m.txt
+copy ic_tg_t18m.txt release\profile\ic_tg_t18m.txt
 copy irecore.exe release\irecore.exe
 copy kernel.c release\kernel.c
 copy gic_zh.qm release\gic_zh.qm

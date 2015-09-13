f48ee125bd7020d690a2502e4943931356b1ec87
Aean <v@aean.net>
2015-09-13 20:35:51 +0800

    nolib package.

--- pack_release.bat
+++ pack_release.bat
@@ -2,17 +2,31 @@
 set qt_dir=E:\Qt\5.5\msvc2013
 
 rd /S /Q release
+rd /S /Q release_nolib
 mkdir release
 mkdir release\profile
 mkdir release\platforms
-copy %windir%\SYSWOW64\msvcr120.dll release\msvcr120.dll
-copy %windir%\SYSWOW64\msvcp120.dll release\msvcp120.dll
+mkdir release_nolib
+mkdir release_nolib\profile
+
+:: Nolib release.
+xcopy profile release_nolib\profile /I
+copy irecore.exe release_nolib\irecore.exe
+copy apltr.exe release_nolib\apltr.exe
+copy kernel.c release_nolib\kernel.c
+copy gic_zh.qm release_nolib\gic_zh.qm
+copy LICENSE release_nolib\LICENSE
+copy lua\lua53.dll release_nolib\lua53.dll
+
+:: Normal release.
 xcopy profile release\profile /I
 copy irecore.exe release\irecore.exe
 copy apltr.exe release\apltr.exe
 copy kernel.c release\kernel.c
 copy gic_zh.qm release\gic_zh.qm
 copy LICENSE release\LICENSE
+copy %windir%\SYSWOW64\msvcr120.dll release\msvcr120.dll
+copy %windir%\SYSWOW64\msvcp120.dll release\msvcp120.dll
 copy %qt_dir%\bin\Qt5Core.dll release\Qt5Core.dll
 copy %qt_dir%\bin\Qt5GUI.dll release\Qt5GUI.dll
 copy %qt_dir%\bin\Qt5Widgets.dll release\Qt5Widgets.dll
